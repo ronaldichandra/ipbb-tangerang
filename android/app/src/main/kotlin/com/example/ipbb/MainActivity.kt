eea8d70bc5935090acd1db4d8c77631d40f7abca
Ronaldi Chandra Karuna <ronaldichdr@gmail.com>
2022-02-25 20:15:32 +0700

    1.1.1

--- android/app/src/main/kotlin/com/example/ipbb/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.ipbb
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ android/app/src/main/kotlin/com/ronaldichandra/ipbb/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ronaldichandra.ipbb
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
